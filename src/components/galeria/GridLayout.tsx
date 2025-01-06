932ad39c3f808613a8fb8b0656962b8981546d6c
Ander <anderhdz06@gmail.com>
2025-01-06 12:33:49 -0600

    Css

--- src/components/galeria/GridLayout.tsx
+++ src/components/galeria/GridLayout.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import './GridLayout.css';
 
-
-
-export default function Galery()  {
+export default function Galery() {
     return (
         <div className="grid-container">
-            <div className="grid-item">
-                <img className='img' src="../public/img/images.jpeg" />
-                <div className="content">
-                    <div className="text"></div>
+            <div className="grid-item bg1">
+                <div className="">
+                    <div className="title">Lorem, ipsum.</div>
                 </div>
             </div>
-            <div className="grid-item">
-                
-                <div className="content">
-                    <div className="line"></div>
+            <div className="grid-item bg2" >
+                <div className="">
+                    <div className="title">Lorem, ipsum.</div>
                 </div>
             </div>
             <div className="grid-item">
@@ -40,4 +36,4 @@ export default function Galery()  {
             </div>
         </div>
     );
-};
+}
