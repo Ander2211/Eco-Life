b7e7ea6adbcd68959cfddf913d9a020ea8277697
Ander <anderhdz06@gmail.com>
2025-01-18 22:16:23 -0600

    Config Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import react from '@astrojs/react';
-
+import vercel from '@astrojs/vercel/serverless'
 import tailwind from '@astrojs/tailwind';
 
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   integrations: [react(), tailwind()],
+  output: 'server',
   adapter: vercel()
 });
